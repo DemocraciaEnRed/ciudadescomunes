dd4896516462fecbb3a3d17b27316eb361bdd652
Guillermo Croppi <guillermocroppi@gmail.com>
2020-04-06 13:51:49 -0300

    New commit with header

---
+++ src/components/home/Intro.vue
@@ -0,0 +1,57 @@
+<template>
+  <section>
+    <!-- <img src="@/assets/img/intro/mobile.svg" class="image intro-header" v-if="showMobile" alt="">
+    <img src="@/assets/img/intro/wide-mobile.svg" class="image intro-header" v-if="showExtraMobile" alt="">
+    <img src="@/assets/img/intro/touch.svg" class="image intro-header" v-if="showTouch" alt="">
+    <img src="@/assets/img/intro/desktop.svg" class="image intro-header" v-if="showDesktop" alt="">
+    <img src="@/assets/img/intro/widescreen.svg" class="image intro-header" v-if="showWidescreen" alt=""> -->
+    <MobileHeader v-if="showMobile" /> 
+    <WideMobileHeader v-if="showExtraMobile" /> 
+    <TouchHeader v-if="showTouch" /> 
+    <DesktopHeader v-if="showDesktop" /> 
+    <WidescreenHeader v-if="showWidescreen" /> 
+  </section>
+</template>
+
+<script>
+import VueScreenSize from 'vue-screen-size'
+import MobileHeader from '@/assets/img/intro/mobile.svg'
+import WideMobileHeader from '@/assets/img/intro/wide-mobile.svg'
+import TouchHeader from '@/assets/img/intro/touch.svg'
+import DesktopHeader from '@/assets/img/intro/desktop.svg'
+import WidescreenHeader from '@/assets/img/intro/widescreen.svg'
+
+export default {
+  components:{
+    MobileHeader,
+    WideMobileHeader,
+    TouchHeader,
+    DesktopHeader,
+    WidescreenHeader
+  },
+  mixins: [VueScreenSize.VueScreenSizeMixin],
+  computed: {
+    showMobile: function(){
+      return this.$vssWidth > 0 && this.$vssWidth <= 480
+    },
+    showExtraMobile: function(){
+      return this.$vssWidth > 480 && this.$vssWidth <= 768
+    },
+    showTouch: function(){
+      return this.$vssWidth > 768 && this.$vssWidth <= 1024
+    },
+    showDesktop: function(){
+      return this.$vssWidth > 1024 && this.$vssWidth <= 1216
+    },
+    showWidescreen: function(){
+      return this.$vssWidth > 1216
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.intro-header{
+  width:100%;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Ciudades comunes - 24 y 25 de Abril 2020',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+  },
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
+    svgRule
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
+  },
+}
