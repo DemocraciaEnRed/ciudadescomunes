cc68bffc31190a2d095ed320d638015b1e96a5db
Guillermo Croppi <guillermocroppi@gmail.com>
2020-04-08 17:50:55 -0300

    Updated with Analytics, new intros, and other fixes

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueGtag from "vue-gtag";
+
+Vue.use(VueGtag, {
+  config: { id: "UA-1234567-1" },
+  params: {
+      send_page_view: false
+    }
+  },
+  router
+);
 
 Vue.config.productionTip = false
 
