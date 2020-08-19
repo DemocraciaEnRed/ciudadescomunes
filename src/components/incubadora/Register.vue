24c8870f20ba56124b7447e47eba3e2be3513b4f
Guillermo Croppi <guillermocroppi@gmail.com>
2020-08-19 20:06:58 -0300

    Updated href Incubadora

--- src/components/incubadora/Register.vue
+++ src/components/incubadora/Register.vue
@@ -5,7 +5,7 @@
         <div class="columns">
           <div class="column is-one-third is-4 has-text-centered-touch">
             <h1 class="title is-4 has-text-black">¿Te interesa estar al tanto de todas las actividades abiertas relacionadas a la incubadora de proyectos de Ciudades Comunes?</h1>
-            <h1 class="title is-3 larger animated pulse slower infinite"><a class="has-text-white" href="https://docs.google.com/forms/d/e/1FAIpQLSfjRwQXGxhviOwRA2ldSE6d3jidk7WBYfTtKSCjbygsB10aag/viewform" target="_blank"><u>suscribite</u></a></h1>
+            <h1 class="title is-3 larger animated pulse slower infinite"><a class="has-text-white" href="https://docs.google.com/forms/d/e/1FAIpQLScDaAmLfhjvcF7mxSaJxZ1jo9Zs-Q17HV_PgSyvUX6vxXEwPg/viewform" target="_blank"><u>suscribite</u></a></h1>
           </div>
           <div class="column is-offset-1 has-text-centered-touch">
             <!-- <h1 class="title is-2 larger is-hidden-touch"><a class="has-text-white" href="https://docs.google.com/forms/d/e/1FAIpQLSe18mgHqJPUof9o4U2z2MOuAaakrmKd7ddhe1-h575etxlyAQ/viewform" target="_blank"><u>inscribite</u></a></h1> -->
