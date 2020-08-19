615e45394cd818005f78e8d5274152ff050b8b96
Guillermo Croppi <guillermocroppi@gmail.com>
2020-08-19 14:23:36 -0300

    streamID updated

--- src/data/incubadoraCalendar.js
+++ src/data/incubadoraCalendar.js
@@ -3,7 +3,7 @@ export default [
     starts: new Date(Date.UTC(2020,7,20,3,0,0)),
     ends: new Date(Date.UTC(2020,7,21,3,0,0)),
     when: new Date(Date.UTC(2020,7,20,21,0,0)),
-    streamId: 'D-UmfqFjpl0',
+    streamId: '83sPnoZIUi4',
     title: 'Inaguración',
     text: [
       '<p clas="is-700">18 hs - Introducción</p>',
@@ -16,7 +16,7 @@ export default [
     starts: new Date(Date.UTC(2020,7,27,3,0,0)),
     ends: new Date(Date.UTC(2020,7,28,3,0,0)),
     when: new Date(Date.UTC(2020,7,27,21,0,0)),
-    streamId: 'D-UmfqFjpl0',
+    streamId: '83sPnoZIUi4',
     title: 'Sesión con invitade 1',
     text: [
       '<p clas="is-700">18 hs - Introducción</p>',
@@ -29,7 +29,7 @@ export default [
     starts: new Date(Date.UTC(2020,8,3,3,0,0)),
     ends: new Date(Date.UTC(2020,8,4,3,0,0)),
     when: new Date(Date.UTC(2020,8,3,21,0,0)),
-    streamId: 'D-UmfqFjpl0',
+    streamId: '83sPnoZIUi4',
     title: 'Sesión con invitade 2',
     text: [
       '<p clas="is-700">18 hs - Introducción</p>',
@@ -42,7 +42,7 @@ export default [
     starts: new Date(Date.UTC(2020,8,5,3,0,0)),
     ends: new Date(Date.UTC(2020,8,6,3,0,0)),
     when: new Date(Date.UTC(2020,8,5,19,0,0)),
-    streamId: 'D-UmfqFjpl0',
+    streamId: '83sPnoZIUi4',
     title: 'Maratón',
     text: [
       '<p clas="is-700">16 hs - Introducción</p>',
@@ -57,7 +57,7 @@ export default [
     starts: new Date(Date.UTC(2020,8,10,3,0,0)),
     ends: new Date(Date.UTC(2020,8,11,3,0,0)),
     when: new Date(Date.UTC(2020,8,10,21,0,0)),
-    streamId: 'D-UmfqFjpl0',
+    streamId: '83sPnoZIUi4',
     title: 'Sesión con invitade 3',
     text: [
       '<p clas="is-700">18 hs - Introducción</p>',
@@ -70,7 +70,7 @@ export default [
     starts: new Date(Date.UTC(2020,8,17,3,0,0)),
     ends: new Date(Date.UTC(2020,8,18,3,0,0)),
     when: new Date(Date.UTC(2020,8,17,21,0,0)),
-    streamId: 'D-UmfqFjpl0',
+    streamId: '83sPnoZIUi4',
     title: 'Sesión con invitade 4',
     text: [
       '<p clas="is-700">18 hs - Introducción</p>',
@@ -83,7 +83,7 @@ export default [
     starts: new Date(Date.UTC(2020,8,19,3,0,0)),
     ends: new Date(Date.UTC(2020,8,20,3,0,0)),
     when: new Date(Date.UTC(2020,8,19,19,0,0)),
-    streamId: 'D-UmfqFjpl0',
+    streamId: '83sPnoZIUi4',
     title: 'Maratón',
     text: [
       '<p clas="is-700">16 hs - Introducción</p>',
@@ -98,7 +98,7 @@ export default [
     starts: new Date(Date.UTC(2020,8,24,3,0,0)),
     ends: new Date(Date.UTC(2020,8,25,3,0,0)),
     when: new Date(Date.UTC(2020,8,24,21,0,0)),
-    streamId: 'D-UmfqFjpl0',
+    streamId: '83sPnoZIUi4',
     title: 'Sesión con invitade 5',
     text: [
       '<p clas="is-700">18 hs - Introducción</p>',
@@ -111,7 +111,7 @@ export default [
     starts: new Date(Date.UTC(2020,9,1,3,0,0)),
     ends: new Date(Date.UTC(2020,9,2,3,0,0)),
     when: new Date(Date.UTC(2020,9,1,21,0,0)),
-    streamId: 'D-UmfqFjpl0',
+    streamId: '83sPnoZIUi4',
     title: 'Sesión con invitade 6',
     text: [
       '<p clas="is-700">18 hs - Introducción</p>',
@@ -124,7 +124,7 @@ export default [
     starts: new Date(Date.UTC(2020,9,29,3,0,0)),
     ends: new Date(Date.UTC(2020,9,30,3,0,0)),
     when: new Date(Date.UTC(2020,9,29,21,0,0)),
-    streamId: 'D-UmfqFjpl0',
+    streamId: '83sPnoZIUi4',
     title: 'Presentacion pública y lanzamiento',
     text: [
       '<p clas="is-700">18 hs - Introducción</p>',
